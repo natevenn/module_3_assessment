9fdd3a2e12274606bc2ef89ad73e62eda560d601
Nathan Venn <natevenn@gmail.com>
2016-04-26 10:02:47 -0600

    testing response of best buy product search

--- app/services/best_buy_service.rb
+++ app/services/best_buy_service.rb
@@ -1,12 +1,13 @@
 class BestBuyService
+  attr_reader :connection
 
   def initialize
-    @connection = Faraday.new("http://api.bestbuy.com/v1/products"
+    @connection = Faraday.new("http://api.bestbuy.com/v1")
     connection.params['apiKey'] = ENV['best_buy_api_key']
   end
 
   def search_products_with_keyword(keyword)
-    parse(connection.get("(longDescription=#{keyword})?pageSize=15&show=name,salePrice,image,customerReviewAverage,shortDescription&format=json"))
+    parse(connection.get("products(longDescription=#{keyword}*)?pageSize=15&show=name,salePrice,image,customerReviewAverage,shortDescription&format=json"))
   end
 
   private
--- spec/services/best_buy_service_spec.rb
+++ spec/services/best_buy_service_spec.rb
@@ -5,12 +5,16 @@ RSpec.describe 'best buy service' do
     VCR.use_cassette 'best_buy_service' do
 
       service = BestBuyService.new
-      result = service.search_products_with_keyword("iphone")
+      results = service.search_products_with_keyword("iphone")
       products = results['products']
+      product_name = products.first.name
+      product_price = product.first.salesPrice
       require "pry"
       binding.pry
 
       expect(products.count).to eq 15
+      expect(product_name).to eq '3D Systems - iSense Portable 3D Scanner - Black/White'
+      expect(product_price).to eq "499.99"
     end
   end
 end
